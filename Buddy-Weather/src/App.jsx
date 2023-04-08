8c39bc2062dd9ea1b20bbaa796f51020b9a6207e
Bogdan-M-source <bogdan.muntean98@gmail.com>
2023-04-08 19:33:28 +0200

    add temp-details

--- Buddy-Weather/src/App.jsx
+++ Buddy-Weather/src/App.jsx
@@ -5,6 +5,7 @@ import './assets/global.css'
 import TopButtons from './components/ui/TopButtons/TopButtons'
 import Inputs from './components/ui/Inputs/Inputs'
 import TimeAndLocation from './components/ui/TimeAndLocation/TimeAndLocation'
+import TemperatureAndDetails from './components/ui/TemperatureAndDetails/TemperatureAndDetails'
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <TopButtons></TopButtons>
       <Inputs></Inputs>
       <TimeAndLocation></TimeAndLocation>
+      <TemperatureAndDetails></TemperatureAndDetails>
+
     </div>
   )
 }
---
+++ Buddy-Weather/src/components/ui/TemperatureAndDetails/TemperatureAndDetails.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./TemperatureAndDetails.css";
+import ImageSun from "../../../assets/weather-images/Sun.png";
+
+function TemperatureAndDetails() {
+  return (
+    <div className="temp-detail-container">
+      <div className='temp-detail-icon-principal' style={{ backgroundImage: `url(${ImageSun})` }}>ewewe</div>
+      TemperatureAndDetails
+    </div>
+  );
+}
+
+export default TemperatureAndDetails;
