e1a8f16bd1db2d3630a48eb32abcb81765f3e5b9
Bogdan-M-source <bogdan.muntean98@gmail.com>
2023-04-23 19:17:06 +0200

    i changed images's dimensions for more quality when are rendered and added README.md with more details about project

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Buddy-Weather
+I have created an application for checking the weather for the current day as well as for the next 7 days or 24 hours.
+
+In variables.scss I used variables to make the code cleaner, efficient and easy to modify.
+In global.scss it addresses the App component.
+
+I applied modularization and structured the components separately, each component with its own scss to be easier to follow and modify.
+The components are divided into form (which serve the inputs in the application) and ui (serve the display of information after the input and the application of the functions)
+
+The functions are divided into data (that process data from the server) and useful (that serve the needs)
+
+The pictures are added to the public folder so that they can be downloaded at the time of deployment. The pictures are dynamically imported and are representative of the weather code for that hour or day.
+
+I used Luxon node package to translate from dt format (date timestamp) or ISO format, to custom format.
+
+###
+In the end, I did the following to deploy:
+-package.json: added at "scripts", "deploy": "gh-pages -d dist"
+- I created the deploy.sh file
+- I installed node packages for github pages
+-in compiler: npm run build + npm run deploy
--- src/App.jsx
+++ src/App.jsx
@@ -11,38 +11,56 @@ function App() {
   const [query, setQuery] = useState({ q: "bucharest" });
   const [units, setUnits] = useState("metric");
   const [weather, setWeather] = useState(null);
+  // const [background, setBackground] = useState("b-default");
 
   useEffect(() => {
     const fetchWeather = async () => {
-      await getFormattedWeatherData({ ...query, units: units }, units).then((data) => {
-        setWeather(data);
-      });
+      await getFormattedWeatherData({ ...query, units: units }, units).then(
+        (data) => {
+          setWeather(data);
+        }
+      );
     };
     fetchWeather();
+    // () => {
+    //   // if(!weather) return 'b-default'
+    //   const limit = units  === 'metric' ? 20 : 60;
+    //   if(weather.temp <= limit){
+    //     console.log("default")
+    //     setBackground('b-default');
+    //   }
+    //   setBackground('b-hot');
+    // }
   }, [query, units]);
   console.log("all data collected until now: ", weather);
-  console.log("units in app.jsx: ", units)
+  console.log("units in app.jsx: ", units);
 
-  const formatBackground = () => {
-    if(!weather) return 'b-default'
-    const limit = units  === 'metric' ? 20 : 60;
-    if(weather.temp <= limit){
-      console.log("default")
-      return 'b-default';
+  const background = () => {
+    if (!weather) return "b-default";
+    const limit = units === "metric" ? 20 : 60;
+    if (weather.temp <= limit) {
+      console.log("default");
+      return("b-default");
     }
-    return 'b-hot';
-  }
+    return("b-hot");
+  };
 
   return (
-    <div className={`App`} id={`${formatBackground()}`}>
+    <div className={`App`} id={`${background()}`}>
       <TopButtons setQuery={setQuery}></TopButtons>
       <Inputs setQuery={setQuery} units={units} setUnits={setUnits}></Inputs>
       {weather && (
         <>
-          <TimeAndLocation weather = {weather}></TimeAndLocation>
-          <TemperatureAndDetails weather = {weather}></TemperatureAndDetails>
-          <Forecast forecastData={weather.hourlyForecast} title="hourly forecast"></Forecast>
-          <Forecast forecastData={weather.dailyForecast} title="daily forecast"></Forecast>
+          <TimeAndLocation weather={weather}></TimeAndLocation>
+          <TemperatureAndDetails weather={weather}></TemperatureAndDetails>
+          <Forecast
+            forecastData={weather.hourlyForecast}
+            title="hourly forecast"
+          ></Forecast>
+          <Forecast
+            forecastData={weather.dailyForecast}
+            title="daily forecast"
+          ></Forecast>
         </>
       )}
     </div>
@@ -50,4 +68,3 @@ function App() {
 }
 
 export default App;
-
